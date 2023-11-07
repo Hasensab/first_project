1ed3c1a8040fc859c6107a7a71192f5a17cb5191
Hasensab <r161135@rguktrkv.ac.in>
2023-11-07 21:15:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import *
+
+# Create your views here.
+def hasen(request):
+    return HttpResponse('hai hasen how is ur software work')
+
+def hai(request):
+    return HttpResponse('<h1 style="color:red"><marquee>hey hasen welcome to webpage</h1></marquee>')
